0d14b366b5ccc4c96f51020f8c09188b936e640d
swapnil <bhaleraoswapnil50@gmail.com>
2022-05-22 07:58:19 +0530

    resolve bugs

--- server.js
+++ server.js
@@ -13,27 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 // simple route
 const db = require("./models");
 db.sequelize.sync();
-const Role = db.role;
 // db.sequelize.sync({force: true}).then(() => {
 //   console.log('Drop and Resync Db');
-//   initial();
-// });
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user"
-  });
- 
-  Role.create({
-    id: 2,
-    name: "moderator"
-  });
- 
-  Role.create({
-    id: 3,
-    name: "admin"
-  });
-}
+//});
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to it solution." });
 });
